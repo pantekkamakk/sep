d0f5c6b251df251089e833e43c7f7c5240b7622f
pantekkamakk <sambal.lecet@kupirlapen.shop>
2024-12-22 13:45:41 -0800

    Update sep10.py

--- sep10.py
+++ sep10.py
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/7a9eab79-1586-4160-a284-72cce30f8766")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/4b339714-8044-4af5-844e-5d66da446fa1")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
