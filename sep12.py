9c70243d7a06dc3a6f79b13ad47dcd97a3c7c28c
pantekkamakk <sambal.lecet@kupirlapen.shop>
2024-12-22 08:12:04 -0800

    Add files via upload

---
+++ sep12.py
@@ -0,0 +1,40 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+options = {
+    'proxy': {
+        'http': 'http://9a88c0f2e53da9ef3be3__cr.sl:70b0113649b957ce@gw.dataimpulse.com:10012',
+        'https': 'https://9a88c0f2e53da9ef3be3__cr.sl:70b0113649b957ce@gw.dataimpulse.com:10012',
+        'no_proxy': 'localhost,127.0.0.1'
+    }
+}
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--verbose")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=1920, 1200")
+# Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
+
+driver.get('https://httpbin.org/ip')
+print(driver.page_source)
+
+time.sleep(1)
+
+driver.get("https://sepolia-faucet.pk910.de/#/mine/7a9eab79-1586-4160-a284-72cce30f8766")
+time.sleep(50)
+
+div_element = driver.find_element(By.CLASS_NAME, "col-3")
+content_text = div_element.text
+print(content_text)
+
+#WAKTU MENUNGGU MINING SELESAI
+time.sleep(15000)
+
+# Tutup browser
+driver.quit()
