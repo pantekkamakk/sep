e674018aaf539048f1966e7e02526e6c56195848
pantekkamakk <sambal.lecet@kupirlapen.shop>
2024-12-22 13:38:29 -0800

    Update sep9.py

--- sep9.py
+++ sep9.py
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/7a9eab79-1586-4160-a284-72cce30f8766")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/f3f92ae2-4f92-437a-95f5-5864fc0d993a")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
