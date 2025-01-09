82da5e5b28b03392f6167e510475315a2627c41d
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-01-09 05:24:43 -0800

    Update sep2.py

--- sep2.py
+++ sep2.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_yYDpudUMzO:4f977a1e-723c-4de4-9053-d392e9bd5cdc@premium-residential.geonode.com:9002',
-        'https': 'https://geonode_yYDpudUMzO:4f977a1e-723c-4de4-9053-d392e9bd5cdc@premium-residential.geonode.com:9002',
+        'http': 'http://ea727252f8dd79eafb23__cr.it:5df1658c8962a3c1@gw.dataimpulse.com:10002',
+        'https': 'https://ea727252f8dd79eafb23__cr.it:5df1658c8962a3c1@gw.dataimpulse.com:10002',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
