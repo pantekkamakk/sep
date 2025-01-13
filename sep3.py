519cfc7294745d879fb889eded5e61afdc15073c
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-01-12 19:43:54 -0800

    Update sep3.py

--- sep3.py
+++ sep3.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/f0a90632-076a-4077-89cb-621ceeed41c5")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/27482a4b-cb45-47da-9453-5457fa6f1693")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
