5507cb30f54c2dca989b9541c99f3fff4c28ec76
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-01-18 23:49:41 -0800

    Update sep4.py

--- sep4.py
+++ sep4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://625b8210c8d223db6b20__cr.ca:8c81014bd125756e@gw.dataimpulse.com:10004',
-        'https': 'https://625b8210c8d223db6b20__cr.ca:8c81014bd125756e@gw.dataimpulse.com:10004',
+        'http': 'http://3e8e59fe8918984c95cc__cr.ca:bd0933404f01b73a@gw.dataimpulse.com:10004',
+        'https': 'https://3e8e59fe8918984c95cc__cr.ca:bd0933404f01b73a@gw.dataimpulse.com:10004',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
