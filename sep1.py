1f197b308bc6940a074b1a3dc5f587f09932eab4
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-01-22 03:10:47 -0800

    Update sep1.py

--- sep1.py
+++ sep1.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://3e8e59fe8918984c95cc__cr.us:bd0933404f01b73a@gw.dataimpulse.com:10101',
-        'https': 'https://3e8e59fe8918984c95cc__cr.us:bd0933404f01b73a@gw.dataimpulse.com:10101',
+        'http': 'http://14c9051125746fecc88c__cr.us:bd0933404f01b73a@gw.dataimpulse.com:10101',
+        'https': 'https://14c9051125746fecc88c__cr.us:bd0933404f01b73a@gw.dataimpulse.com:10101',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
