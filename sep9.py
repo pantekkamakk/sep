6b1894a194c8d9306478029948267cbd07cd3280
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-02-11 15:13:09 -0800

    Update sep9.py

--- sep9.py
+++ sep9.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://0f7e52f041d9094b7ee9__cr.ar:edf6b8890bb720a9@gw.dataimpulse.com:10009',
-        'https': 'https://0f7e52f041d9094b7ee9__cr.ar:edf6b8890bb720a9@gw.dataimpulse.com:10009',
+        'http': 'http://b2071d9164a5f81672e8__cr.ar:a6f8d0086f5012ff@gw.dataimpulse.com:10009',
+        'https': 'https://b2071d9164a5f81672e8__cr.ar:a6f8d0086f5012ff@gw.dataimpulse.com:10009',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
