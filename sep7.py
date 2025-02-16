c791957356085d324f5621b3e2de6bcb5906ac8b
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-02-16 08:56:03 -0800

    Update sep7.py

--- sep7.py
+++ sep7.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://8040cd35b165bcecfccc__cr.th:1f949a7f21e45b33@gw.dataimpulse.com:10107',
-        'https': 'https://8040cd35b165bcecfccc__cr.th:1f949a7f21e45b33@gw.dataimpulse.com:10107',
+        'http': 'http://7d33bcbc5566fbd20662__cr.th:5b0cf16d90a92244@gw.dataimpulse.com:10107',
+        'https': 'https://7d33bcbc5566fbd20662__cr.th:5b0cf16d90a92244@gw.dataimpulse.com:10107',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
