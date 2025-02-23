5ac612aa78f502be70e0ddde27304596bc7968fa
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-02-23 09:26:31 -0800

    Update sep11.py

--- sep11.py
+++ sep11.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://90b6ecf47722e4db8966__cr.ca:097ab82fe400fe51@gw.dataimpulse.com:10111',
-        'https': 'https://90b6ecf47722e4db8966__cr.ca:097ab82fe400fe51@gw.dataimpulse.com:10111',
+        'http': 'http://sodagar:FGAMRHJ-QQHC7SJ-0RPJ3H1-BOMNLIP-WKACAQO-GWSCKMZ-SET2MG4@unmetered.residential.proxyrack.net:10077',
+        'https': 'https://sodagar:FGAMRHJ-QQHC7SJ-0RPJ3H1-BOMNLIP-WKACAQO-GWSCKMZ-SET2MG4@unmetered.residential.proxyrack.net:10077',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
