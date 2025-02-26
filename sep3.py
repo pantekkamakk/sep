5aa67233bb92577a33e28266c647df73600292d6
pantekkamakk <sambal.lecet@kupirlapen.shop>
2025-02-26 12:41:11 -0800

    Update sep3.py

--- sep3.py
+++ sep3.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://sodagar:FGAMRHJ-QQHC7SJ-0RPJ3H1-BOMNLIP-WKACAQO-GWSCKMZ-SET2MG4@unmetered.residential.proxyrack.net:10069',
-        'https': 'https://sodagar:FGAMRHJ-QQHC7SJ-0RPJ3H1-BOMNLIP-WKACAQO-GWSCKMZ-SET2MG4@unmetered.residential.proxyrack.net:10069',
+        'http': 'http://ea727252f8dd79eafb23__cr.cr:5df1658c8962a3c1@gw.dataimpulse.com:10003',
+        'https': 'https://ea727252f8dd79eafb23__cr.cr:5df1658c8962a3c1@gw.dataimpulse.com:10003',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
